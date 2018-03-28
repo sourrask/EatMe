76b7c42668efa3bde835aaf9898b7838ca34f2a6
Desahri <didier96@live.nl>
2018-03-28 23:18:18 +0200

    activities now working

--- app/src/main/java/kon/demo/favoritesActivity.java
+++ app/src/main/java/kon/demo/favoritesActivity.java
@@ -17,7 +17,7 @@ import data.Recipe;
  * We also use the back button of the phone to return to the homescreen.
  */
 public class favoritesActivity extends AppCompatActivity {
-    // Gets the template for the favorites activity by setting the content view to the desired layout
+    // Gets the template for the favorites activity by setting the content view to a desired layout
     private Context context;
     private ControlPanel controlpanel;
 
